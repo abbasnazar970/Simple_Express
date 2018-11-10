1d9dd698f15bc4d004bb716be72b4787248140f3
Abbas Nazar <abbasnazar.970@gmail.com>
2018-11-10 18:14:50 +0500

    partials

--- server.js
+++ server.js
@@ -3,20 +3,28 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getCurrentYear',()=>{
+   return  new Date().getFullYear()
+})
+hbs.registerHelper('screamIt',(ab)=>{
+    return ab.toUpperCase()
+ })
 app.set('view engine','hbs');
 
 app.use(express.static(__dirname+'/dist'));
 
 app.get('/',(req,res)=>{
     //res.send("<h1>Hello there</h1>");
-    res.render('home.hbs');
+    res.render('home.hbs',{
+        'title' : 'About Page'
+    });
 });
 
 app.get('/about',(req,res)=>
 {
     res.render('about.hbs',{
-        'title' : 'About Page',
-        'currentYear' : new Date().getFullYear()
+        'title' : 'About Page'
     });
 });
 
